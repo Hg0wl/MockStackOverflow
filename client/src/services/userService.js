7194cf3e4ca5ba6b4f5166af7332ea0da5676d56
Nicholas Soares <nsoares22@lasalle-academy.org>
2024-04-13 19:36:23 -0400

    Edit User Profile
    Can Edit usernames entirely. Front-end is there for changing the profile picture, but Imgur rejects incoming requests from localhost, so need to find a workaround or wait until we use Docker

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,10 +4,44 @@ const USER_API_URL = `${REACT_APP_API_URL}/user`;
 
 // To get Questions by id
 const getUserById = async (uid) => {
-    console.log("Calling get question by id!")
+  console.log("Calling get question by id!");
   const res = await api.get(`${USER_API_URL}/getUserById/${uid}`);
 
   return res.data;
 };
 
-export { getUserById }
+const uploadImage = (file) => {
+  console.log(file);
+  console.log(file.type.match(/image.*/));
+  /* Is the file an image? */
+  if (!file || !file.type.match(/image.*/)) return;
+
+  /* It is! */
+  document.body.className = "uploading";
+
+  /* Lets build a FormData object*/
+  var fd = new FormData(); // I wrote about it: https://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
+  fd.append("image", file); // Append the file
+  var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+  xhr.open("POST", "https://api.imgur.com/3/image.json"); // Boooom!
+  xhr.onload = function () {
+    // Big win!
+    console.log(JSON.parse(xhr.responseText).data.link);
+  };
+
+  xhr.setRequestHeader("Authorization", "Client-ID 486a4056dc8671d");
+
+  /* And now, we send the formdata */
+  xhr.send(fd);
+
+  console.log(fd);
+};
+
+const updateUsername = async (username, uid) => {
+    const res = await api.post(`${USER_API_URL}/changeUsername`, {
+      username: username,
+      uid: uid
+    });
+    return res.data
+}
+export { getUserById, uploadImage, updateUsername };
--- server/controller/user.js
+++ server/controller/user.js
@@ -10,6 +10,29 @@ const getUserById = async (req, res) => {
   res.send(user);
 };
 
+const changeUsername = async (req, res) => {
+  let newUsername = req.body.username;
+  let uid = req.body.uid;
+  console.log(newUsername);
+
+  let userList = await User.find({ username: { $eq: newUsername } });
+  if (userList.length == 0) {
+    await User.findOneAndUpdate(
+      { _id: { $eq: uid } },
+      { $set: { username: newUsername } },
+      { new: true }
+    );
+
+    res.send({ success: true });
+  } else {
+    res.send({
+      success: false,
+      message: "A user with this username already exists",
+    });
+  }
+};
+
 router.get("/getUserById/:uid", (req, res) => getUserById(req, res));
+router.post("/changeUsername", (req, res) => changeUsername(req, res));
 
 module.exports = router;
